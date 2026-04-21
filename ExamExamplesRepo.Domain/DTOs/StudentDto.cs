8d86accbea3ae00f4cfb2a1a0d26bc141c55e511
ruheHowest <rune.herreman@student.howest.be>
2026-04-21 18:03:01 +0200

    Add a DTO and interface using generics

---
+++ ExamExamplesRepo.Domain/DTOs/StudentDto.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ExamExamplesRepo.Domain.DTOs
+{
+    public record StudentDto(
+        int Id,
+        string FullName,
+        IEnumerable<string> Courses
+    );
+
+    public record PagedResponse<T>(IEnumerable<T> Data,
+        int TotalCount,
+        int PageNumber,
+        int PageSize
+    )
+    {
+        public int TotalPages => (int)Math.Ceiling((double)TotalCount / PageSize);
+    }
+}
---
+++ ExamExamplesRepo.Domain/Interfaces/IRepository.cs
@@ -0,0 +1,19 @@
+﻿using ExamExamplesRepo.Infrastruture.Models;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ExamExamplesRepo.Domain.Interfaces
+{
+    public interface IRepository<T> where T: class
+    {
+        Task<IEnumerable<T>> GetAllAsync(CancellationToken ct);
+        Task<T?> GetByIdAsync(int id, CancellationToken ct);
+    }
+
+    public interface IStudentRepository
+        : IRepository<Student>
+    {
+        Task<(IEnumerable<Student> Students, int Total)> GetPagesStudentsAsync(int page, int pageSize, CancellationToken ct);
+    }
+}
